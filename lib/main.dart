f480910d61905db0d31820a875dc5d8a0728257e
jake1808 <51802968+jake1808@users.noreply.github.com>
2021-03-13 19:36:29 +0200

    first commit
    complete project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//the main function is the starting point for all our Flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text(
+            'I Am Rich',
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
